79cc94855517b80c16a12ce0f24d2a2aae2f7261
MuskanGupta1503 <66381138+MuskanGupta1503@users.noreply.github.com>
2020-09-26 13:11:37 +0530

    added 4th variable

--- index.py
+++ index.py
@@ -2,24 +2,29 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 
-year = [2014, 2015, 2016, 2017, 2018, 2019]  
+year = [ 1 , 2 , 3 , 4 , 5 , 6]
 tutorial_premium = [0, 0, 13, 56, 39, 14]
 tutorial_public = [39, 117, 98, 54, 28, 15]  
 tutorial_public1 = [39, 117, 98, 54, 28, 15]  
 
 b_tp1=list(np.add(tutorial_premium,tutorial_public))
 
+l4 = [30, 10, 98, 50, 20, 10]
+
+b4=list(np.add(b_tp1,tutorial_public1))
+
 plt.barh(year, tutorial_premium, color="#f3e151")  
 # careful: notice "bottom" parameter became "left"
 plt.barh(year, tutorial_public, left=tutorial_premium, color="#6c3376")
-plt.barh(year, tutorial_public1, left=b_tp1, color="#f3e151") 
+plt.barh(year, tutorial_public1, left=b_tp1, color="red") 
+plt.barh(year, l4, left=b4, color="black") 
 
 # we also need to switch the labels
 plt.xlabel('Number of futurestud.io Tutorials')  
-plt.ylabel('Year')
+plt.ylabel('Vehicle Number')
 
-plt.show()  
-
+plt.show()
+plt.save()
 
 # https://futurestud.io/tutorials/matplotlib-stacked-bar-plots
 # https://matplotlib.org/3.1.3/gallery/lines_bars_and_markers/horizontal_barchart_distribution.html
